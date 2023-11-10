414405ce8051e9977a471011e6bab16ebcf0d4f0
Katerrynaa <katadmitrenkoxx@gmail.com>
2023-11-10 23:53:59 +0100

    Rewrited code

--- main.py
+++ main.py
@@ -2,28 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-fruits = {
-    'orange': '9',
-    'strawberry': '10',
-    'banana': '3',
-    'peach': '5',
-    'raspberry': '19',
-    'grape': '4'
-}
-
 @app.get('/fruits')
 def read_fruits():
-    return fruits
-
-
-departments = {
-    'HR': 'Norway',
-    'IT Support': 'Sweden',
-    'Achitecture': 'Poland',
-    'Sales': 'USA',
-    'Design': 'Denmark',
-}
+    return {
+	    'orange': '9',
+	    'strawberry': '10',
+	    'banana': '3',
+	    'peach': '5',
+	    'raspberry': '19',
+	    'grape': '4'
+	}
 
 @app.get('/departments')
 def read_depart():
-    return departments
+    return {
+        'HR': 'Norway',
+        'IT Support': 'Sweden',
+        'Achitecture': 'Poland',
+        'Sales': 'USA',
+        'Design': 'Denmark',
+    }
