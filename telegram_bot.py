154ea0730849b2af7d4bfafcfc90b3cf9d2d9dc4
Katerrynaa <katadmitrenkoxx@gmail.com>
2023-11-13 18:08:16 +0100

    removed file

--- telegram_bot.py
+++
@@ -1,8 +0,0 @@
-from telegram.ext import Updater, Update, CallbackContext, CommandHandler, MessageHandler, Filters
-
-updater = Updater("6524720663:AAEXM5cc7ve0DN9gTDy7yKbyOjGQPXSTAc4", use_context=True)
-
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        'Hello! Welcome to the Chat Bot. You got into the volunteers team who help ukrainian develop and bring our country closer to vicroty.'
-    )
