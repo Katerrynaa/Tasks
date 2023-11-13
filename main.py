95d01543610064fc8182268ad72ef8db0a7015cc
Katerrynaa <katadmitrenkoxx@gmail.com>
2023-11-13 19:37:17 +0100

    delete a few lines of code

--- main.py
+++ main.py
@@ -2,17 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/fruits')
-def read_fruits():
-    return {
-	    'orange': '9',
-	    'strawberry': '10',
-	    'banana': '3',
-	    'peach': '5',
-	    'raspberry': '19',
-	    'grape': '4'
-	}
-
 @app.get('/departments')
 def read_depart():
     return {
