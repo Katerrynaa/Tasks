f08beb88a36d40f515d3ffe9eb62cc3b3cbb77be
Yuniia Olkhova <yuniia.olkhova@pfizer.com>
2023-11-27 18:09:44 +0200

    Read config and run app

--- main.py
+++ main.py
@@ -1,64 +1,6 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, Session, declarative_base
+from src.config import read_config
 
-DATABASE_URL = "mysql+mysqlconnector://root:rootroot11@localhost:3306/departments"
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-
-class Department(Base):
-    __tablename__ = "departments"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(200))
-    country_name = Column(String(100))
-
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-def insert_data(db: Session):
-    department_data = [
-        {"title": "HR", "country_name": "Norway"},
-        {"title": "IT Support", "country_name": "Sweden"},
-        {"title": "Achitecture", "country_name": "Poland"},
-        {"title": "Sales", "country_name": "USA"},
-    ]
-
-    for data in department_data:
-        department = Department(**data)
-        db.add(department)
-        db.commit()
-
-
-@app.put("/departments")
-def push_info():
-    with SessionLocal() as session:
-        insert_data(session)
-        return {"Data added successfully"}
-
-
-@app.get("/departments")
-def read_depart(db: Session = Depends(get_db)):
-    departments = db.query(Department).all()
-    if not departments:
-        raise HTTPException(status_code=404, detail="Departments table not found")
-    return departments
-
-
-@app.get("departments/{department_id}")
-def get_id(department_id: int, db: Session = Depends(get_db)):
-    departments = db.query(Department).filter(Department.id == department_id).all()
-    return {"department_id": department_id}
+if __name__ == '__main__':
+    config = read_config()
+    # create_engine
+    # run app with read config
--- src/config.py
+++ src/config.py
@@ -1,15 +1,12 @@
-import configparser
+from dataclasses import dataclass
+
+from dotenv import dotenv_values
 
 
-def read(path):
-    config = configparser.ConfigParser()
-    config.read(path)
-    return config
+def read_config():
+    return Config(**dotenv_values())
 
 
-def get_db(config):
-    return config.get(
-        "database",
-        "DATABASE_URL",
-        fallback="mysql://root:rootroot11@localhost:3306/departments",
-    )
+@dataclass
+class Config:
+    DATABASE_URL: str
