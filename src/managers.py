2c075cfe08f5e0a162403ac9a6e920dfa0b73390
Katerrynaa <katadmitrenkoxx@gmail.com>
2023-12-05 15:01:22 +0100

    chore: Fix methods names

--- src/managers.py
+++ src/managers.py
@@ -3,16 +3,16 @@ from src.models import Department, SessionLocal
 
 class DepartmentManager:
     @staticmethod
-    def insert_data(data: dict):
+    def create(data: dict):
         with SessionLocal() as session:
             session.add(Department(**data))
 
     @staticmethod
-    def get_info():
+    def get_all():
         with SessionLocal() as session:
             return session.query(Department).all()
 
     @staticmethod
-    def get_id(dep_id):
+    def get_by_id(department_id):
         with SessionLocal() as session:
-            return session.query(Department).filter_by(id=dep_id).first()
+            return session.query(Department).filter_by(id=department_id).first()
