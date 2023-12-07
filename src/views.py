eeaf94c32b9c04ac80fe162298513f3af234a925
Katerrynaa <katadmitrenkoxx@gmail.com>
2023-12-07 14:39:17 +0100

    style: Update name functions

--- src/views.py
+++ src/views.py
@@ -5,18 +5,18 @@ router = APIRouter(prefix="/departments")
 
 
 @router.post("/")
-def create_department(department: dict):
+def create(department: dict):
     DepartmentManager.insert_data(data=department)
     return {"Data added successfully"}
 
 
 @router.get("/", name="get_departments")
-def read_departments():
+def get_all():
     return DepartmentManager.get_info()
 
 
 @router.get("/{dep_id}")
-def get_department(dep_id: int):
+def get_by_id(dep_id: int):
     if not (department := DepartmentManager.get_id(dep_id)):
         raise HTTPException(status_code=404, detail="Departments id table not found")
     return department
