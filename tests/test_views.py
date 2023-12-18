d2d6507744e0ac5d941be17fd5a34fba4d3079b0
Katerrynaa <katadmitrenkoxx@gmail.com>
2023-12-18 22:50:41 +0100

    chore: Create tests for views

--- tests/test_views.py
+++ tests/test_views.py
@@ -23,3 +23,43 @@ class TestCreateDepartmentView:
 
     def test_status_code(self, create_department):
         assert create_department.status_code == 200
+
+
+class TestGetDepartmentView:
+    @fixture(scope="class")
+    def manager_get_all_mock(self):
+        with patch.object(DepartmentManager, "get_all") as mock:
+            yield mock
+
+    @fixture(scope="class", autouse=True)
+    def get_all_department(self, test_client, manager_get_all_mock):
+        return test_client.get("/departments")
+    
+    def test_manager_called(self, manager_get_all_mock):
+        manager_get_all_mock.assert_called_once_with()
+
+    def test_result(self, get_all_department):
+        assert get_all_department.json()  == { }
+
+    def test_status_code(self, get_all_department):
+        assert get_all_department.status_code == 200
+
+class TestGetIdDepartmentView:
+    @fixture(scope="class")
+    def manager_get_by_id_mock(self):
+        with patch.object(DepartmentManager, "get_by_id") as mock:
+            yield mock
+
+    @fixture(scope="class", autouse=True)
+    def get_by_id_department(self, test_client, manager_get_by_id_mock):
+        return test_client.get("/{dep_id}")
+    
+    def test_manager_called(self, manager_get_by_id_mock, dep_id: dict):
+        manager_get_by_id_mock.assert_called_once_with(dep_id['id'])
+
+    def test_result(self, get_by_id_department):
+        assert get_by_id_department.json().get('detail', '').casefold() == 'not found'
+
+    def test_status_code(self, get_by_id_department):
+        assert get_by_id_department.status_code == 404
+
