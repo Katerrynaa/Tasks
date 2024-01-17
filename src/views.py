09e2bfbc0550a52ead3e6bf6255427322c078909
Katerrynaa <katadmitrenkoxx@gmail.com>
2024-01-17 12:16:55 +0100

    Create middleware

--- src/views.py
+++ src/views.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 from src.managers import DepartmentManager
 
 router = APIRouter(prefix="/departments")
@@ -9,11 +9,21 @@ def create(department: dict):
     DepartmentManager.create(data=department)
     return "Data added successfully"
 
+@router.middleware("http")
+async def print_hello(request: Request, call_next):
+    print("Hello")
+    response = await call_next(request)
+    return response 
 
 @router.get("/", name="get_departments")
 def get_all():
     return DepartmentManager.get_all()
 
+@router.middleware("http")
+async def print_goodbye(request: Request, call_next):
+    print("Goodbye")
+    response = await call_next(request)
+    return response
 
 @router.get("/{department_id}")
 def get_by_id(department_id: int):
