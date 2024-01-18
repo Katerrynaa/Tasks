26ee5a6023d290b9a459d257d7ecf7914fbb35f8
Katerrynaa <katadmitrenkoxx@gmail.com>
2024-01-18 13:56:51 +0100

    Delete middleware

--- src/views.py
+++ src/views.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, HTTPException
 from src.managers import DepartmentManager
 
 router = APIRouter(prefix="/departments")
@@ -10,23 +10,9 @@ def create(department: dict):
     return "Data added successfully"
 
 
-@router.middleware("http")
-async def print_hello(request: Request, call_next):
-    print("Hello")
-    response = await call_next(request)
-    return response
-
-
 @router.get("/", name="get_departments")
 def get_all():
     return DepartmentManager.get_all()
-
-
-@router.middleware("http")
-async def print_goodbye(request: Request, call_next):
-    print("Goodbye")
-    response = await call_next(request)
-    return response
 
 
 @router.get("/{department_id}")
