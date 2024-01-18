add01268f899e22b51d826f9ed0da8bea298caee
Katerrynaa <katadmitrenkoxx@gmail.com>
2024-01-18 14:05:08 +0100

    Code formatting

--- src/middleware.py
+++ src/middleware.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+
 @app.middleware("http")
 async def print_hello(request: Request, call_next):
     print("Hello")
     response = await call_next(request)
     print("Goodbye")
     return response
-
-
-
