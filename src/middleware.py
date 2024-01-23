d1500c3d46f0b81cde959938614a4f7e56958f1d
Katerrynaa <katadmitrenkoxx@gmail.com>
2024-01-23 18:38:46 +0100

    Delete middleware decorator

--- src/middleware.py
+++ src/middleware.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI, Request
 
-app = FastAPI()
-
-
-@app.middleware("http")
 async def print_hello(request: Request, call_next):
     print("Hello")
     response = await call_next(request)
