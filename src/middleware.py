95973f7e34dc0a1dd097511c805d6e308ebd3eed
Katerrynaa <katadmitrenkoxx@gmail.com>
2024-01-25 16:32:15 +0100

    Create context variables

--- src/middleware.py
+++ src/middleware.py
@@ -1,8 +1,22 @@
 from fastapi import Request
+from src.models import SessionLocal, Department
+import contextvars
 
 
-async def print_hello(request: Request, call_next):
-    print("Hello")
+async def start_session(data: dict):
+    session_var = contextvars.ContextVar("session")
+    async with SessionLocal() as session:
+        with session.begin():
+            session_var.set(session)
+            obj = Department(**data)
+            session.add(obj)
+            session.flush()
+            session.refresh(obj)
+            session.expunge_all()
+            return obj
+
+
+async def reset_variable(request: Request, call_next, session_var):
     response = await call_next(request)
-    print("Goodbye")
+    session_var.reset()
     return response
