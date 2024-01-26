23e67f66d73f93b0d0864fac3a2f71d90411f078
Katerrynaa <katadmitrenkoxx@gmail.com>
2024-01-26 16:52:25 +0100

    Improve session management in function

--- src/middleware.py
+++ src/middleware.py
@@ -1,22 +1,11 @@
 from fastapi import Request
-from src.models import SessionLocal, Department
-import contextvars
+from src.models import SessionLocal, session_var
 
 
-async def start_session(data: dict):
-    session_var = contextvars.ContextVar("session")
+async def start_session(request: Request, call_next):
     async with SessionLocal() as session:
         with session.begin():
             session_var.set(session)
-            obj = Department(**data)
-            session.add(obj)
-            session.flush()
-            session.refresh(obj)
-            session.expunge_all()
-            return obj
-
-
-async def reset_variable(request: Request, call_next, session_var):
-    response = await call_next(request)
-    session_var.reset()
-    return response
+            response = await call_next(request)
+            session_var.reset()
+            return response
