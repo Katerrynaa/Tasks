6e2b37d7b4f31b80f9794219b4be02467e68c4a7
Kateryna <katadmitrenkoxx@gmail.com>
2024-03-14 15:51:18 +0100

    Remove debug print

--- src/middleware.py
+++ src/middleware.py
@@ -3,7 +3,6 @@ from src.models import SessionLocal, session_var
 
 
 async def start_session(request: Request, call_next):
-    print("\n\n\n\n", "Hello", "\n\n\n\n")
     with SessionLocal() as session:
         with session.begin():
             token = session_var.set(session)
